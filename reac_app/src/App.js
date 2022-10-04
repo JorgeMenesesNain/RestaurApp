4a19352bebbdf129ff072b8b7119e44e0729ca4a
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-03 22:03:08 -0300

    implemenmtacion de semantic

---
+++ reac_app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <Button primary>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
