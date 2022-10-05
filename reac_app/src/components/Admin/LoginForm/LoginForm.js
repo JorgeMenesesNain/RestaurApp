f98f9e6e039b4d69e0491172f20c2ce8774e0e63
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-05 08:48:28 -0300

    Estado y validacion del formularios de login

--- reac_app/src/components/Admin/LoginForm/LoginForm.js
+++ reac_app/src/components/Admin/LoginForm/LoginForm.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import {Button, Form} from "semantic-ui-react"
+import {useFormik} from "formik"
+import * as Yup from "yup"
 import "./LoginForm.scss"
 
 export function LoginForm() {
+  const fromik =useFormik ({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit:(formValue)=>{
+      console.log('Email enviado');
+      console.log(formValue);
+    }
+  })
   return (
-    <Form className='login-form-admin'>
+    <Form className='login-form-admin' onSubmit={fromik.handleSubmit}>
       <Form.Input 
-      name = "email"
-      placeholder="Correo electronico"
+      name = "email" 
+      placeholder="Correo electronico" 
+      value = {fromik.values.email} 
+      onChange= {fromik.handleChange}
+      error={fromik.errors.email}
       />
       <Form.Input 
-      name = "password"
-      type='password'
-      placeholder = "Contraseña"
+      name = "password" 
+      type='password' 
+      placeholder = "Contraseña" 
+      value = {fromik.values.password}
+      onChange= {fromik.handleChange}
+      error={fromik.errors.password}
       />
       <Button 
       type='submit' 
       content="Iniciar sessión" 
-      primary fluid 
-      />
+      primary fluid />
     </Form>
   )
+}
+
+function initialValues(){
+  return {
+    email :"",
+    password: ""
+  };
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string().email("Se requiere Correo correcto").required("Se requiere Correo"),
+    password: Yup.string().required("Se requiere contraserña"),
+  };
 }
