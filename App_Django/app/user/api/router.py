039591c750c9888fd28ecde1db3ec5473eaafd34
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-06 00:49:39 -0300

    Creando endpoint de login con JWT en django

--- App_Django/app/user/api/router.py
+++ App_Django/app/user/api/router.py
@@ -1,6 +1,7 @@
 from django.urls import URLPattern, path
 from rest_framework.routers import DefaultRouter
 from user.api.views import UserApiViewSet, UserView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 router_user = DefaultRouter()
 
@@ -10,5 +11,6 @@ router_user.register(
 
 
 urlpatterns = [
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/me/', UserView.as_view())
 ]
