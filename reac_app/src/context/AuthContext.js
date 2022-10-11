3c12d1ccd088f051274f07848ba8b54ee9034e4e
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-11 20:11:50 -0300

    logout del context

--- reac_app/src/context/AuthContext.js
+++ reac_app/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React ,{useState, useEffect, createContext} from "react";
-import {setToken,getToken} from "../api/token";
+import {setToken,getToken,removeToken} from "../api/token";
 import {useUser} from "../hooks"
 
 export const AuthContext = createContext({
@@ -7,7 +7,6 @@ export const AuthContext = createContext({
     login: () => null,
     logout: () => null
 }) ;
-
 
 export function AuthProvaider(props){
     const { children } = props;
@@ -23,22 +22,27 @@ export function AuthProvaider(props){
             }else{
                 setAuth(null)
             }
-        })()
-    },[])
+        })();
+    },[]);
 
     const login = async (token) => {
         setToken(token)
-        //console.log('Context login ------>', token);
         const me =await getME(token);
         setAuth({token, me});
-        console.log(me);
-    }
+    };
+
+    const logout = () => {
+        if(auth){
+            removeToken();
+            setAuth(null);
+        }
+    };
 
     const valueContext = {
         auth,
         login,
-        logout: () => console.log('Cerrando session'),
-    }
+        logout,
+    };
 
     if(auth === undefined) return null;
 
