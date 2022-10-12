aff2d8b9e6142fab206cf6bc8013484b039b113e
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-12 01:05:51 -0300

    Logica y menus del SideMenu

--- reac_app/src/components/Admin/SideMenu/SideMenu.js
+++ reac_app/src/components/Admin/SideMenu/SideMenu.js
@@ -1,24 +1,48 @@
 import React from 'react'
-import {Menu, Icon} from "semantic-ui-react"
+import {Menu, Icon} from "semantic-ui-react";
+import {Link, useLocation} from "react-router-dom"
 import "./SideMenu.scss"
 
 export function SideMenu(props) {
     const {children} = props;
+    const {pathname} = useLocation();
   return (
     <div className='side-menu-admin'>
-      <MenuLeft />
+      <MenuLeft pathname={pathname}/>
       <div className='content'>{children}</div>
     </div>
   );
 }
 
 function MenuLeft(props){
-    const {} = props;
+    const {pathname} = props;
     return (
         <Menu fixed='left' borderless className='side' vertical>
-            <Menu.Item>
+            {/* -------------------------Home ----------------------------*/}
+            <Menu.Item as={Link} to={'/admin'} active={pathname === "/admin"}>
                 <Icon name='home' />Pedidos
             </Menu.Item>
+            {/* ----------------------------Mesas---------------------------- */}
+            <Menu.Item as={Link} to={'/admin/tables'} active={pathname === "/admin/tables"}>
+                <Icon name='table' />Mesas
+            </Menu.Item>
+            {/* ----------------------------Historial de pagos---------------------------- */}
+            <Menu.Item as={Link} to={'/admin/payments-history'} active={pathname === "/admin/payments-history"}>
+                <Icon name='history' />Historial de pagos
+            </Menu.Item>
+            {/* ----------------------------Categorias---------------------------- */}
+            <Menu.Item as={Link} to={'/admin/categories'} active={pathname === "/admin/categories"}>
+                <Icon name='folder' />Categorias
+            </Menu.Item>
+            {/* ----------------------------Productos---------------------------- */}
+            <Menu.Item as={Link} to={'/admin/products'} active={pathname === "/admin/products"}>
+                <Icon name='cart' />Productos
+            </Menu.Item>
+            {/* ----------------------------Usuarios---------------------------- */}
+            <Menu.Item as={Link} to={'/admin/users'} active={pathname === "/admin/users"}>
+                <Icon name='users' />Usuarios
+            </Menu.Item>
+
         </Menu>
     );
 }
