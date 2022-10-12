4dc429fdb90a2ab3ec14fe7464513a9ead576841
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-12 01:25:40 -0300

    Restringir la sección de usuarios

--- reac_app/src/components/Admin/SideMenu/SideMenu.js
+++ reac_app/src/components/Admin/SideMenu/SideMenu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Menu, Icon} from "semantic-ui-react";
 import {Link, useLocation} from "react-router-dom"
+import {useAuth} from "../../../hooks"
 import "./SideMenu.scss"
 
 export function SideMenu(props) {
@@ -16,6 +17,7 @@ export function SideMenu(props) {
 
 function MenuLeft(props){
     const {pathname} = props;
+    const {auth} = useAuth()
     return (
         <Menu fixed='left' borderless className='side' vertical>
             {/* -------------------------Home ----------------------------*/}
@@ -39,10 +41,11 @@ function MenuLeft(props){
                 <Icon name='cart' />Productos
             </Menu.Item>
             {/* ----------------------------Usuarios---------------------------- */}
-            <Menu.Item as={Link} to={'/admin/users'} active={pathname === "/admin/users"}>
-                <Icon name='users' />Usuarios
-            </Menu.Item>
-
+            {auth.me?.is_staff &&(
+                <Menu.Item as={Link} to={'/admin/users'} active={pathname === "/admin/users"}>
+                    <Icon name='users' />Usuarios
+                </Menu.Item>
+            )}
         </Menu>
     );
 }
