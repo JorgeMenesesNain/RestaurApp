479a400c08ac0f51fa4a0a32085c9484e0d33e65
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-13 13:16:04 -0300

    Creando el ModelViewSet de categorias

--- App_Django/app/app/urls.py
+++ App_Django/app/app/urls.py
@@ -22,6 +22,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from user.api.router import router_user
+from categories.api.router import router_category
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -44,4 +45,5 @@ urlpatterns = [
                                         cache_timeout=0), name='schema-redoc'),
     path('api/', include(router_user.urls)),
     path('api/', include('user.api.router')),
+    path('api/', include (router_category.urls)),
 ]
