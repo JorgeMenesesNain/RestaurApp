1e2d53509269906f217d985fa3b1dab05e5480f0
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-13 18:19:28 -0300

    Estado y validacion del formulario

--- reac_app/src/components/Admin/Category/AddEditCategoryForm/AddEditCategoryForm.js
+++ reac_app/src/components/Admin/Category/AddEditCategoryForm/AddEditCategoryForm.js
@@ -1,12 +1,26 @@
 import React, { useState, useCallback } from "react";
 import { Form, Image, Button } from "semantic-ui-react";
 import { useDropzone } from "react-dropzone";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./AddEditCategoryForm.scss";
 
 export function AddEditCategoryForm() {
   const [previewImage, setPreviewImage] = useState(null);
-  const onDrop = useCallback((acceptedFile) => {
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(newSchema()),
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      console.log("Formulario enviado");
+      console.log(formValue);
+    },
+  });
+
+  const onDrop = useCallback(async (acceptedFile) => {
     const file = acceptedFile[0];
+    await formik.setFieldValue("image", file);
     setPreviewImage(URL.createObjectURL(file));
   }, []);
 
@@ -18,10 +32,21 @@ export function AddEditCategoryForm() {
   });
 
   return (
-    <Form className="add-edit-category-form">
-      <Form.Input name="title" placeholder="Nombre de la categoria" />
+    <Form className="add-edit-category-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="title"
+        placeholder="Nombre de la categoria"
+        value={formik.values.titleo}
+        onChange={formik.handleChange}
+        error={formik.errors.title}
+      />
 
-      <Button type="button" fluid {...getRootProps()}>
+      <Button
+        type="button"
+        fluid
+        color={formik.errors.image && "red"}
+        {...getRootProps()}
+      >
         Subir imagen
       </Button>
       <input {...getInputProps()} />
@@ -30,4 +55,18 @@ export function AddEditCategoryForm() {
       <Button type="submit" primary fluid content="Crear" />
     </Form>
   );
+}
+
+function initialValues() {
+  return {
+    title: "",
+    image: "",
+  };
+}
+
+function newSchema() {
+  return {
+    title: Yup.string().required(true),
+    image: Yup.string().required(true),
+  };
 }
