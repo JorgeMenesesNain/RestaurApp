3113e10481c7f63c3bca1d2f2b4049d73aef1e87
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-13 14:47:06 -0300

    Pagina de catagorias

---
+++ reac_app/src/pages/Admin/CategoriesAdmin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeaderPage } from "../../components/Admin";
+
+export function CategoriesAdmin() {
+  return (
+    <>
+      <HeaderPage title="Categorias" btnTitle="Nueva categoria" />
+    </>
+  );
+}
--- reac_app/src/routes/routes.admin.js
+++ reac_app/src/routes/routes.admin.js
@@ -1,19 +1,24 @@
-import {AdminLayout} from "../Layouts"
-import {HomeAdmin, UsersAdmin} from "../pages/Admin"
+import { AdminLayout } from "../Layouts";
+import { HomeAdmin, UsersAdmin, CategoriesAdmin } from "../pages/Admin";
 
 const routesAdmin = [
-    {
-        path: "/admin",
-        layout: AdminLayout,
-        component: HomeAdmin,
-        exact: true
-
-    },
-    {
-        path: "/admin/users",
-        layout: AdminLayout,
-        component: UsersAdmin,
-        exact: true
-    },
+  {
+    path: "/admin",
+    layout: AdminLayout,
+    component: HomeAdmin,
+    exact: true,
+  },
+  {
+    path: "/admin/users",
+    layout: AdminLayout,
+    component: UsersAdmin,
+    exact: true,
+  },
+  {
+    path: "/admin/categories",
+    layout: AdminLayout,
+    component: CategoriesAdmin,
+    exact: true,
+  },
 ];
 export default routesAdmin;
