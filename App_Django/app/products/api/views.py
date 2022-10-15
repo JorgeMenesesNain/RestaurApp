278a01a11cb61ab7a983787d7eea688f66122d21
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-15 04:17:35 -0300

    Sistema de filtros

--- App_Django/app/products/api/views.py
+++ App_Django/app/products/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-""" from django_filters.rest_framework import DjangoFilterBackend """
+from django_filters.rest_framework import DjangoFilterBackend 
 
 from products.models import Product
 from products.api.serializers import ProductSerializer
@@ -10,5 +10,5 @@ class ProductApiViewSet(ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
-    """ filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category', 'active'] """
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'active']
