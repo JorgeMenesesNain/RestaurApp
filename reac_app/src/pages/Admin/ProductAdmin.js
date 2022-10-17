bddd265563533243aedc0058a1aabf07e5f09ef5
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-17 18:12:05 -0300

    Pagina de productos

---
+++ reac_app/src/pages/Admin/ProductAdmin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeaderPage } from "../../components/Admin";
+
+export function ProductAdmin() {
+  return (
+    <>
+      <HeaderPage title="Productos" btnTitle="Nuevo producto" />
+    </>
+  );
+}
