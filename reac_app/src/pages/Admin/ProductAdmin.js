3a35729e93ce953089dae27bcaef8191d6a6ecac
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-17 18:30:31 -0300

    Obteniendo todos los productos

--- reac_app/src/pages/Admin/ProductAdmin.js
+++ reac_app/src/pages/Admin/ProductAdmin.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Loader } from "semantic-ui-react";
 import { HeaderPage } from "../../components/Admin";
+import { useProduct } from "../../hooks";
 
 export function ProductAdmin() {
+  const { loading, products, getProducts } = useProduct();
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <>
       <HeaderPage title="Productos" btnTitle="Nuevo producto" />
+      {loading ? (
+        <Loader active inline="centered">
+          Cargando...
+        </Loader>
+      ) : (
+        <p>Products Taable</p>
+      )}
     </>
   );
 }
