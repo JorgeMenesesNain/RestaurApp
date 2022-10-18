e11067c2ccbabdeac9fe6c8777b6ba02a2a428ba
JorgeMenesesNain <jo.menesesn@alumnos.duoc.cl>
2022-10-18 15:18:07 -0300

    Obteniendo mesas

--- reac_app/src/pages/Admin/TablesAdmin.js
+++ reac_app/src/pages/Admin/TablesAdmin.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Loader } from "semantic-ui-react";
+import { useTable } from "../../hooks";
 import { HeaderPage } from "../../components/Admin";
 
 export function TablesAdmin() {
+  const { loading, tables, getTables } = useTable();
+
+  useEffect(() => {
+    getTables();
+  }, []);
+
+  console.log(tables);
   return (
     <>
       <HeaderPage title="Mesas" btnTitle="Crear nueva Mesa" />
+
+      {loading ? (
+        <Loader active inline="centered">
+          Cargando...
+        </Loader>
+      ) : (
+        <h2>Lista de mesas</h2>
+      )}
     </>
   );
 }
