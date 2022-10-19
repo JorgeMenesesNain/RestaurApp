4222f52dd4f74c97f4208576357f4fd1712c5350
LilXander21 <greengaz8@gmail.com>
2022-10-19 19:50:00 -0300

    Obteniendo los pedidos de una mesa

--- reac_app/src/components/Admin/Table/TableAdmin/TableAdmin.js
+++ reac_app/src/components/Admin/Table/TableAdmin/TableAdmin.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Icon, Checkbox } from "semantic-ui-react";
 import { ReactComponent as IcTable } from "../../../../assets/table.svg";
+import { getOrdersByTableApi } from "../../../../api/orders";
+import { ORDER_STATUS } from "../../../../utils/constants";
 import "./TableAdmin.scss";
 
 export function TableAdmin(props) {
   const { table } = props;
+
+  useEffect(
+    () => {
+      (async () => {
+        console.log(table.id);
+        const response = await getOrdersByTableApi(
+          table.id,
+          ORDER_STATUS.PENDING
+        );
+        console.log(response);
+        /* setOrders(response); */
+      })();
+    },
+    [
+      /* reload */
+    ]
+  );
+
   return (
     <div className="table-admin">
       <IcTable />
