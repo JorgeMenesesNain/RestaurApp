6ea168959a69c2ec5b10f0d5c960a8e67da74e10
LilXander21 <greengaz8@gmail.com>
2022-10-19 19:29:33 -0300

    Últimos detalles a la vista de la mesas

--- reac_app/src/components/Admin/Table/TablesListAdmin/TablesListAdmin.js
+++ reac_app/src/components/Admin/Table/TablesListAdmin/TablesListAdmin.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Button, Icon, Checkbox } from "semantic-ui-react";
 import { map } from "lodash";
 import { TableAdmin } from "../";
 import "./TablesListAdmin.scss";
@@ -8,6 +8,20 @@ export function TablesListAdmin(props) {
   const { tables } = props;
   return (
     <div className="tables-list-admin">
+      <Button
+        primary
+        icon
+        className="tables-list-admin__reaload"
+        onClick={() => console.log("onRedetchLoad")}
+      >
+        <Icon name="refresh" />
+      </Button>
+
+      <div className="tables-list-admin__reaload-toggle">
+        <span>Relaod automatico</span>
+        <Checkbox toggle onChange={(_, data) => console.log(data.checked)} />
+      </div>
+
       {map(tables, (table) => (
         <TableAdmin key={table.number} table={table} />
       ))}
