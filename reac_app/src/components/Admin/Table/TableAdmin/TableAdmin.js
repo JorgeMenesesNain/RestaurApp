e1edbb235ac183e9e25bf1441ebe4a668443de97
LilXander21 <greengaz8@gmail.com>
2022-10-19 20:11:19 -0300

    Marcando las mesa que tiene pedidos pendientes

--- reac_app/src/components/Admin/Table/TableAdmin/TableAdmin.js
+++ reac_app/src/components/Admin/Table/TableAdmin/TableAdmin.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Button, Icon, Checkbox } from "semantic-ui-react";
+import { Label } from "semantic-ui-react";
+import { size } from "lodash";
+import classNames from "classnames";
 import { ReactComponent as IcTable } from "../../../../assets/table.svg";
 import { getOrdersByTableApi } from "../../../../api/orders";
 import { ORDER_STATUS } from "../../../../utils/constants";
@@ -7,17 +9,16 @@ import "./TableAdmin.scss";
 
 export function TableAdmin(props) {
   const { table } = props;
+  const [orders, setOrders] = useState([]);
 
   useEffect(
     () => {
       (async () => {
-        console.log(table.id);
         const response = await getOrdersByTableApi(
           table.id,
           ORDER_STATUS.PENDING
         );
-        console.log(response);
-        /* setOrders(response); */
+        setOrders(response);
       })();
     },
     [
@@ -27,7 +28,16 @@ export function TableAdmin(props) {
 
   return (
     <div className="table-admin">
-      <IcTable />
+      {size(orders) > 0 ? (
+        <Label circular color="orange">
+          {size(orders)}
+        </Label>
+      ) : null}
+      <IcTable
+        className={classNames({
+          pending: size(orders) > 0,
+        })}
+      />
       <p>Mesa {table.number} </p>
     </div>
   );
