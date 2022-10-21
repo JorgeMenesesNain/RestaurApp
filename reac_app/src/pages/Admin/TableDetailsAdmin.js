64116a894e309c3c1256477ed9ba5119b98ab6be
BlackitoS <bravosjaime@gmail.com>
2022-10-20 23:18:52 -0300

    Obteniendo los pedidos de una mesa

--- reac_app/src/pages/Admin/TableDetailsAdmin.js
+++ reac_app/src/pages/Admin/TableDetailsAdmin.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Loader } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+import { HeaderPage } from "../../components/Admin";
+import { useOrder } from "../../hooks";
 
 export function TableDetailsAdmin() {
+  const { id } = useParams();
+  const { loading, orders, getOrdersByTable } = useOrder();
+
+  useEffect(() => {
+    getOrdersByTable(id);
+  }, []);
+
   return (
-    <div>
-      <h2>Detalle de la mesa</h2>
-    </div>
+    <>
+      <HeaderPage title={`Mesa ****`} />
+      {loading ? (
+        <Loader active inline="centered">
+          Cargando ...
+        </Loader>
+      ) : (
+        <h2>Lista de pedidos</h2>
+      )}
+    </>
   );
 }
