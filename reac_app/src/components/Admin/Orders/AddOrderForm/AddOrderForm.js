ea8615c3a2e0ee8fcfdeb86c92ecb4f98c8e6076
BlackitoS <bravosjaime@gmail.com>
2022-10-21 01:35:45 -0300

    Obteniendo productos para el dropdown

--- reac_app/src/components/Admin/Orders/AddOrderForm/AddOrderForm.js
+++ reac_app/src/components/Admin/Orders/AddOrderForm/AddOrderForm.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Image, Button, Dropdown } from "semantic-ui-react";
+import { map } from "lodash";
+import { useProduct, useOrder } from "../../../../hooks";
 import "./AddOrderForm.scss";
 
 export function AddOrderForm(props) {
   const { idTable, openCloseModal } = props;
+  const [productsFormat, setProductsFormat] = useState([]);
+  const { products, getProducts } = useProduct();
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  useEffect(() => {
+    setProductsFormat(formatDropdownData(products));
+  }, [products]);
+
   return (
     <Form className="add-order-form">
-      <Dropdown placeholder="Productos" fluid selection search />
+      <Dropdown
+        placeholder="Productos"
+        fluid
+        selection
+        search
+        options={productsFormat}
+      />
       <div className="add-order-form__list">
         {/* for de prtoductos seleccionado */}
       </div>
@@ -19,4 +37,12 @@ export function AddOrderForm(props) {
       />
     </Form>
   );
+}
+
+function formatDropdownData(data) {
+  return map(data, (item) => ({
+    key: item.id,
+    text: item.title,
+    value: item.id,
+  }));
 }
