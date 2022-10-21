d79dc978c3f67eeb756ddb6468e261e57f898e09
BlackitoS <bravosjaime@gmail.com>
2022-10-21 00:09:38 -0300

    Formatiendo las fechas de los pedidos

--- reac_app/src/components/Admin/TableDetails/OrderItemAdmin/OrderItemAdmin.js
+++ reac_app/src/components/Admin/TableDetails/OrderItemAdmin/OrderItemAdmin.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button, Image } from "semantic-ui-react";
 import classNames from "classnames";
+import moment from "moment";
+import "moment/locale/es";
 import "./OrderItemAdmin.scss";
 
 export function OrderItemAdmin(props) {
@@ -12,7 +14,10 @@ export function OrderItemAdmin(props) {
         [order.status.toLowerCase()]: true,
       })}
     >
-      <div className="order-item-admin__time">{order.created_at}</div>
+      <div className="order-item-admin__time">
+        <span>{moment(order.created_at).format("HH:mm")}</span> {" - "}
+        <span>{moment(order.created_at).startOf("secods").fromNow()}</span>
+      </div>
       <div className="order-item-admin__product">
         <Image src={image} />
         <p>{title}</p>
