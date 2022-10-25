1921e5603683bae00a494e1a0d541049ea97663e
BlackitoS <bravosjaime@gmail.com>
2022-10-25 14:29:59 -0300

    Creando screen y ruta

---
+++ reac_app/src/pages/Admin/PaymentsHistory.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeaderPage } from "../../components/Admin";
+
+export function PaymentsHistory() {
+  return (
+    <div>
+      <HeaderPage title="Hitorial de pagos" />
+    </div>
+  );
+}
