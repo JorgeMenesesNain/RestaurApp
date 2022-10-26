5e23117e4043e265616b3b12de6d9f67ffedcb89
LilXander21 <greengaz8@gmail.com>
2022-10-26 15:20:49 -0300

    Obtener los productos del carrito

--- reac_app/src/pages/Client/Cart.js
+++ reac_app/src/pages/Client/Cart.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useProduct } from "../../hooks";
+import { getProductsCart } from "../../api/cart";
 
 export function Cart() {
+  const [products, setProducts] = useState(null);
+  const { getProductById } = useProduct();
+
+  console.log(products);
+
+  useEffect(() => {
+    (async () => {
+      const idProductsCart = getProductsCart();
+
+      const productsArray = [];
+      for await (const idProduct of idProductsCart) {
+        const response = await getProductById(idProduct);
+        productsArray.push(response);
+      }
+      setProducts(productsArray);
+    })();
+  }, []);
+
   return (
     <div>
       <h1>Carrito</h1>
