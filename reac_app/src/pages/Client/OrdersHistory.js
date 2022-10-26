7e903c1b6626c683b3efb6b0e9016b50ef127499
LilXander21 <greengaz8@gmail.com>
2022-10-26 17:59:58 -0300

    Obteniendo los pedidos de una mesa

--- reac_app/src/pages/Client/OrdersHistory.js
+++ reac_app/src/pages/Client/OrdersHistory.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useOrder, useTable, usePayment } from "../../hooks";
 
 export function OrdersHistory() {
+  const { loading, orders, getOrdersByTable, addPaymentToOrder } = useOrder();
+  const { getTableByNumber } = useTable();
+  const { tableNumber } = useParams();
+
+  console.log(orders);
+
+  useEffect(() => {
+    (async () => {
+      const table = await getTableByNumber(tableNumber);
+      const idTableTemp = table[0].id;
+
+      getOrdersByTable(idTableTemp, "", "ordering=-status,-created_at");
+    })();
+  }, []);
+
   return (
     <div>
       <h1>Historial de pedidos</h1>
