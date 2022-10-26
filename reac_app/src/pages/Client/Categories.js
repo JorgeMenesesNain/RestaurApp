8323ce83aefbfba43958a71ac0e654ac34f2802f
LilXander21 <greengaz8@gmail.com>
2022-10-26 13:54:09 -0300

    Obteniendo las categorias

--- reac_app/src/pages/Client/Categories.js
+++ reac_app/src/pages/Client/Categories.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useCategory } from "../../hooks";
 
 export function Categories() {
+  const { loading, categories, getCategories } = useCategory();
+
+  useEffect(() => {
+    getCategories();
+  }, []);
   return (
     <div>
-      <h3>Categories</h3>
-      <p>Lista de categorias</p>
+      <h3>Categorias</h3>
+      {loading ? <p>Cargando</p> : <h2>Lista de categhorias</h2>}
     </div>
   );
 }
