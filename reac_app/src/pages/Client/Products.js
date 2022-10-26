083709cac49fc1a8e0715c7bd0a6e3df80d5a302
LilXander21 <greengaz8@gmail.com>
2022-10-26 14:27:30 -0300

    Obteniendo los productos de una categoria

--- reac_app/src/pages/Client/Products.js
+++ reac_app/src/pages/Client/Products.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
+import { useProduct } from "../../hooks";
 
 export function Products() {
   const { tableNumber, idCategory } = useParams();
+  const { loading, products, getProductsByCategory } = useProduct();
 
+  useEffect(() => {
+    getProductsByCategory(idCategory);
+  }, [idCategory]);
   return (
     <div>
       <Link to={`/client/${tableNumber}`}>Volver a categorias</Link>
-      <h1>Products</h1>
+      {loading ? <p>Cargando...</p> : <h2>Productos</h2>}
     </div>
   );
 }
