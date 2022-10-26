9a8770b3aca8690c2b9a58316588a3055528df0f
LilXander21 <greengaz8@gmail.com>
2022-10-26 15:31:49 -0300

    Vista cuando el carrito esta vacio

--- reac_app/src/pages/Client/Cart.js
+++ reac_app/src/pages/Client/Cart.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "semantic-ui-react";
+import { Link, useParams } from "react-router-dom";
+import { size } from "lodash";
 import { useProduct } from "../../hooks";
 import { getProductsCart } from "../../api/cart";
 
 export function Cart() {
   const [products, setProducts] = useState(null);
   const { getProductById } = useProduct();
-
-  console.log(products);
+  const { tableNumber } = useParams();
 
   useEffect(() => {
     (async () => {
@@ -24,6 +26,18 @@ export function Cart() {
   return (
     <div>
       <h1>Carrito</h1>
+      {!products ? (
+        <p>Cargando...</p>
+      ) : size(products) === 0 ? (
+        <div style={{ textAlign: "center" }}>
+          <p>Tu carrito esta vacio</p>
+          <Link to={`/client/${tableNumber}/orders`}>
+            <Button primary>Ver pedidos</Button>
+          </Link>
+        </div>
+      ) : (
+        <p>Lista de productos</p>
+      )}
     </div>
   );
 }
