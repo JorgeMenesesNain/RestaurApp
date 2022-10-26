69c5429db20aa4e705800ce9580dd329c9e7a5f9
LilXander21 <greengaz8@gmail.com>
2022-10-26 17:15:49 -0300

    Generando pedido

--- reac_app/src/components/Client/ListProductCart/ListProductCart.js
+++ reac_app/src/components/Client/ListProductCart/ListProductCart.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Image, Button, Icon } from "semantic-ui-react";
 import { map, forEach } from "lodash";
 import { useParams, useHistory } from "react-router-dom";
-import { removeProductCartApi } from "../../../api/cart";
+import { useOrder, useTable } from "../../../hooks";
+import { removeProductCartApi, cleanProductCartApi } from "../../../api/cart";
 import "./ListProductCart.scss";
 
 export function ListProductCart(props) {
   const { products, onReloadCart } = props;
   const [total, setTotal] = useState(0);
+  const { addOrderToTable } = useOrder();
+  const { getTableByNumber } = useTable();
+  const { tableNumber } = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     let totalTemp = 0;
@@ -22,6 +27,17 @@ export function ListProductCart(props) {
     onReloadCart();
   };
 
+  const createOrder = async () => {
+    const tableData = await getTableByNumber(tableNumber);
+    const idTable = tableData[0].id;
+
+    for await (const product of products) {
+      await addOrderToTable(idTable, product.id);
+    }
+    cleanProductCartApi();
+    history.push(`/client/${tableNumber}/orders`);
+  };
+
   return (
     <div className="list-product-cart">
       {map(products, (product, index) => (
@@ -34,7 +50,7 @@ export function ListProductCart(props) {
           <Icon name="close" onClick={() => removeProduct(index)} />
         </div>
       ))}
-      <Button primary fluid>
+      <Button primary fluid onClick={createOrder}>
         Realizar pedido (${total})
       </Button>
     </div>
